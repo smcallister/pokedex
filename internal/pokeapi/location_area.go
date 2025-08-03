1ad054a6b301dac12d41625397878448818b6978
smcallister <stevenkmcallister@gmail.com>
2025-08-03 09:41:59 -0400

    Implemented explore, catch, inspect, and pokedex commands

--- internal/pokeapi/location_area.go
+++ internal/pokeapi/location_area.go
@@ -20,6 +20,59 @@ type LocationAreaPage struct {
 	}                   `json:"results"`
 }
 
+type LocationArea struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string 				`json:"name"`
+			URL  string 				`json:"url"`
+		} 								`json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int 				`json:"rate"`
+			Version struct {
+				Name string 			`json:"name"`
+				URL  string 			`json:"url"`
+			} 							`json:"version"`
+		} 								`json:"version_details"`
+	} 									`json:"encounter_method_rates"`
+	GameIndex int 						`json:"game_index"`
+	ID        int 						`json:"id"`
+	Location  struct {
+		Name string 					`json:"name"`
+		URL  string 					`json:"url"`
+	} 									`json:"location"`
+	Name  string 						`json:"name"`
+	Names []struct {
+		Language struct {
+			Name string 				`json:"name"`
+			URL  string 				`json:"url"`
+		} 								`json:"language"`
+		Name string 					`json:"name"`
+	} 									`json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string 				`json:"name"`
+			URL  string 				`json:"url"`
+		} 								`json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int   	`json:"chance"`
+				ConditionValues []any 	`json:"condition_values"`
+				MaxLevel        int   	`json:"max_level"`
+				Method          struct {
+					Name string 		`json:"name"`
+					URL  string 		`json:"url"`
+				} 						`json:"method"`
+				MinLevel int 			`json:"min_level"`
+			} 							`json:"encounter_details"`
+			MaxChance int 				`json:"max_chance"`
+			Version   struct {
+				Name string 			`json:"name"`
+				URL  string 			`json:"url"`
+			} 							`json:"version"`
+		} 								`json:"version_details"`
+	} 									`json:"pokemon_encounters"`
+}
+
 const LocationAreaURL = "https://pokeapi.co/api/v2/location-area"
 
 var cache = pokecache.NewCache(time.Duration(10 * time.Minute))
@@ -66,4 +119,51 @@ func GetLocationAreas(url string) (*LocationAreaPage, error) {
 	// Cache the result and return the page.
 	cache.Add(url, body)
 	return &page, nil
+}
+
+func GetLocationArea(name string) (*LocationArea, error) {
+	// Construct the URL for the specific location area.
+	url := fmt.Sprintf("%s/%s", LocationAreaURL, name)
+
+	// Check the cache first.
+	if cached, found := cache.Get(url); found {
+		var area LocationArea
+		if err := json.Unmarshal(cached, &area); err != nil {
+			return nil, err
+		}
+	
+		return &area, nil
+	}
+
+	// Query for the specific location area.
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Failed to fetch location area: %s", resp.Status)
+	}
+
+	// Convert the result to a LocationArea struct.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(body) == 0 {
+		return nil, fmt.Errorf("Empty response body for location area")
+	}
+
+	// Decode the JSON response into a LocationArea struct.
+	var area LocationArea
+	err = json.Unmarshal(body, &area)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cache the result and return the area.
+	cache.Add(url, body)
+	return &area, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 
@@ -17,10 +18,11 @@ type cmdConfig struct {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*cmdConfig) error
+	callback    func(*cmdConfig, ...string) error
 }
 
 var commands map[string]cliCommand
+var pokedex map[string]*pokeapi.Pokemon
 
 func cleanInput(text string) []string {
 	var cleaned []string
@@ -36,7 +38,7 @@ func cleanInput(text string) []string {
 	return cleaned
 }
 
-func commandHelp(config *cmdConfig) error {
+func commandHelp(config *cmdConfig, args ...string) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
@@ -47,7 +49,7 @@ func commandHelp(config *cmdConfig) error {
 	return nil
 }
 
-func commandMap(config *cmdConfig) error {
+func commandMap(config *cmdConfig, args ...string) error {
 	// Get the next page of location areas.
 	if config.next == nil {
 		fmt.Println("you're on the last page")
@@ -68,7 +70,7 @@ func commandMap(config *cmdConfig) error {
 	return nil
 }
 
-func commandMapB(config *cmdConfig) error {
+func commandMapB(config *cmdConfig, args ...string) error {
 	// Get the previous page of location areas.
 	if config.previous == nil {
 		fmt.Println("you're on the first page")
@@ -91,7 +93,97 @@ func commandMapB(config *cmdConfig) error {
 	return nil
 }
 
-func commandExit(config *cmdConfig) error {
+func commandExplore(config *cmdConfig, args ...string) error {
+	// Check if a location area is specified.
+	if len(args) == 0 {
+		return fmt.Errorf("Please specify a location area to explore")
+	}
+
+	// Get the location area.
+	fmt.Printf("Exploring %s\n", args[0])
+	area, err := pokeapi.GetLocationArea(args[0])
+	if err != nil {
+		return err
+	}
+
+	// Print the details of the location area.
+	fmt.Println("Found Pokemon:");
+	for _, encounter := range area.PokemonEncounters {
+		fmt.Printf("- %s\n", encounter.Pokemon.Name)
+	}
+
+	return nil
+}
+
+func commandCatch(config *cmdConfig, args ...string) error {
+	// Check if a pokemon is specified.
+	if len(args) == 0 {
+		return fmt.Errorf("Please specify a pokemon to catch")
+	}
+
+	// Get the pokemon.
+	pokemon, err := pokeapi.GetPokemon(args[0])
+	if err != nil {
+		return err
+	}
+
+	// Try to catch the pokemon.
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+	if rand.Intn(100000) > pokemon.BaseExperience {
+		fmt.Printf("%s was caught!\n", pokemon.Name)
+		fmt.Println("You may now inspect it with the inspect command.")
+		pokedex[pokemon.Name] = pokemon
+	} else {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+	}
+
+	return nil
+}
+
+func commandInspect(config *cmdConfig, args ...string) error {
+	// Check if a pokemon is specified.
+	if len(args) == 0 {
+		return fmt.Errorf("Please specify a pokemon to inspect")
+	}
+
+	// Get the pokemon from the Pokedex.
+	pokemon, exists := pokedex[args[0]]
+	if !exists {
+		return fmt.Errorf("you have not caught that pokemon")
+	}
+
+	// Print the details of the pokemon.
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("- %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, typeInfo := range pokemon.Types {
+		fmt.Printf("- %s\n", typeInfo.Type.Name)
+	}
+
+	return nil
+}
+
+func commandPokedex(config *cmdConfig, args ...string) error {
+	if len(pokedex) == 0 {
+		fmt.Println("Your Pokedex is empty. Catch some Pokemon first!")
+		return nil
+	}
+
+	fmt.Println("Your Pokedex:")
+	for name := range pokedex {
+		fmt.Printf("- %s\n", name)
+	}
+
+	return nil
+}
+
+func commandExit(config *cmdConfig, args ...string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!");
 	os.Exit(0)
 	return nil
@@ -115,12 +207,34 @@ func main() {
 			description: "Displays the names of the previous 20 location areas in the Pokemon world",
 			callback:    commandMapB,
 		},
+		"explore": {
+			name:        "explore <area name>",
+			description: "Explore a location area in the Pokemon world",
+			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch <pokemon name>",
+			description: "Catch a pokemon and add it to your Pokedex",
+			callback:    commandCatch,
+		},
+		"inspect": {
+			name:        "inspect <pokemon name>",
+			description: "Inspects a pokemon in your Pokedex",
+			callback:    commandInspect,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "Prints the names of all pokemon in your Pokedex",
+			callback:    commandPokedex,
+		},
     	"exit": {
         	name:        "exit",
         	description: "Exit the Pokedex",
         	callback:    commandExit,
     	},
 	}
+
+	pokedex = make(map[string]*pokeapi.Pokemon)
 
 	url := pokeapi.LocationAreaURL
 	config := cmdConfig{
@@ -145,8 +259,8 @@ func main() {
 
 		// Look up the command and execute it.
 		if command, exists := commands[cleanedWords[0]]; exists {
-			if err := command.callback(&config); err != nil {
-				fmt.Printf("Error executing command: %v\n", err)
+			if err := command.callback(&config, cleanedWords[1:]...); err != nil {
+				fmt.Printf("%v\n", err)
 			}
 		} else {
 			fmt.Printf("Unknown command: %s\n", cleanedWords[0])
